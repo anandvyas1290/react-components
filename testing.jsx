71d997b554a6dbd8f7fbed5de48452fc47f63387
anand-vyas-gg <anand.vyas@globalgarner.com>
2023-10-04 14:30:02 +0530

    testing env setup

--- testing.jsx
+++ testing.jsx
@@ -3,9 +3,14 @@ import { createRoot } from "react-dom/client";
 import { PrimaryBtn, SecondaryBtn, SecondaryBtn2 } from "./src";
 import "./src/index.css";
 
-createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <PrimaryBtn>Ok</PrimaryBtn> <SecondaryBtn>Ok</SecondaryBtn>{" "}
-        <SecondaryBtn2>Ok</SecondaryBtn2>
-    </React.StrictMode>
-);
+let mode = import.meta.env.VITE_MODE;
+
+mode === "development"
+    ? createRoot(document.getElementById("root")).render(
+          <React.StrictMode>
+              <PrimaryBtn>Ok</PrimaryBtn>
+              <SecondaryBtn>Ok</SecondaryBtn>
+              <SecondaryBtn2>Ok</SecondaryBtn2>
+          </React.StrictMode>
+      )
+    : null;
--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,14 @@
 import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
         lib: {
-            // Could also be a dictionary or array of multiple entry points
-            entry: resolve(__dirname, "lib/main.js"),
-            name: "MyLib",
-            // the proper extensions will be added
-            fileName: "my-lib",
+            entry: "src/index.js",
+            name: "react-wind-components",
         },
-        rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled
-            // into your library
-            external: ["vue"],
-            output: {
-                // Provide global variables to use in the UMD build
-                // for externalized deps
-                globals: {
-                    vue: "Vue",
-                },
-            },
-        },
+        rollupOptions: {},
     },
     plugins: [react()],
 });
