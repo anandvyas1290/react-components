b5612c9bd3c628f6efc61d87dce9a950b8936343
anand-vyas-gg <anand.vyas@globalgarner.com>
2023-10-04 18:47:49 +0530

    working on build setup

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
     build: {
         lib: {
-            entry: "src/index.js",
+            entry: "src/index.jsx",
             name: "react-wind-components",
         },
         rollupOptions: {},
