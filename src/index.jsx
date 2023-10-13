11a1d885375beda44630949faaca9354cc7eee5a
anand-vyas-gg <anand.vyas@globalgarner.com>
2023-10-13 17:43:20 +0530

    changes in build settings and added message component

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
 import "./index.css";
 
 export * from "./lib/components";
+export * from "./lib/ui";
--- testing.jsx
+++ testing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Button } from "./src";
-import "./src/index.css";
+// import "./src/index.css";
 
 let mode = import.meta.env.VITE_MODE;
 
