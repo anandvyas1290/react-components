f4c5384fd07af0ba30f383248beb424bdb763884
anand-vyas-gg <anand.vyas@globalgarner.com>
2024-05-16 16:02:55 +0530

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./lib/**/*.{js,ts,jsx,tsx}"],
+    content: ["./lib/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
             colors: {
